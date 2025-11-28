09b522ba06765594843daeec8e74c1ec9728d93c
MARIA BAÑULS RIBES <m.banulsribes@edu.gva.es>
2025-11-28 11:41:25 +0100

    Update Collatz.java

--- src/main/java/gvaCEED/Collatz.java
+++ src/main/java/gvaCEED/Collatz.java
@@ -3,19 +3,48 @@ import java.util.Scanner;
 
 public class Collatz {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-   
 
-        // ✅ 1. Demanar un numero positiu
-       
-        // ✅ 2. Guardar el numero inicial
+        Scanner entrada = new Scanner(System.in) ;
 
-        // ✅ 3.Mostra
+        int nUsuario;
+        
+        
+        while (true) {
+            System.out.print("Introduce un numero entero positivo: ");
+            if( entrada.hasNextInt()) {
+                nUsuario = entrada.nextInt();
+                if ( nUsuario > 0) {
+                    break;
+                    
+                } else {
+                    System.out.println("Numero introducido no valido");
+                    entrada.next();
+                }
+            } 
+        }  
 
-        // ✅ 4. Mostrar la seqüència amb while
+        int primerNumero = nUsuario; 
+        System.out.println("\n Secuencia de Collatz para el numero: "+ primerNumero);
 
-        // ✅ 5. Mostrar el numero total de passos
+        int contadorPasos = 0;
+        System.out.print(primerNumero);
+        
+        while (primerNumero != 1) {
+            if(primerNumero % 2 == 0) {
+                primerNumero = primerNumero/2;
+            
+                }else {
+                    primerNumero = (primerNumero * 3) + 1;
+                }
+            System.out.print(" - " + primerNumero);
+            contadorPasos++;
+        }
+
+        System.out.println("\nNumero total de pasos realizados: "+ contadorPasos);
+         
+        
+        entrada.close(); 
     }
-
-} 
+}
+   
 
